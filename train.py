9259e04c88f50f93cedffe50995517e0ec790f1b
SYJ <1638846513@qq.com>
2024-01-12 20:10:08 +0800

    Update train.py

--- train.py
+++ train.py
@@ -14,8 +14,8 @@ seed=42
 set_seed(seed)
 
 ### Loading data
-st_adata = sc.read_h5ad('/public/home/syj/data/st-seq/osmFISH.h5ad')
-sc_adata = sc.read_h5ad('/public/home/syj/data/scRNA-seq/Zeisel.h5ad')
+st_adata = sc.read_h5ad('dataset/st-seq/osmFISH.h5ad')
+sc_adata = sc.read_h5ad('dataset/scRNA-seq/Zeisel.h5ad')
 emb_file = 'embed/osmFISH_emb.pkl' # emb_file = None
 
 raw_spatial_df = pd.DataFrame(st_adata.X, columns=st_adata.var_names)
@@ -37,6 +37,8 @@ for train_ind, test_ind in kf.split(raw_shared_gene):
     scrna_df   = raw_scrna_df
     all_pred_res[test_gene], all_reliable_res[test_gene] = scPread(spatial_df, scrna_df, train_gene, test_gene, seed=seed, emb_file=emb_file)
     idx += 1
+    
+pickle.dump(all_pred_res, open('scPread.pkl', 'wb'))
 
 ### Calculating metrics
 print('result: ')
